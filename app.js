edf8e126880af5de0b9d8a7a6ff73308fe3ef821
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-08-22 18:34:35 -0500

    first comit

---
+++ app.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoute')
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iyvq7.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => { 
+    console.log(`DB connected: ${result.connection.host}`);
+    app.listen(process.env.PORT)
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
