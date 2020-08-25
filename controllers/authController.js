426512fb5bfdbae6d2951e2900f6200aa8978552
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-08-24 19:16:37 -0500

    handle errors on front-end

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,8 +32,8 @@ module.exports.signup_get = (req, res) => {
 }
 
 module.exports.login_get = (req, res) => {
-   res.render('login')
-}
+   res.render('login');
+};
 
 module.exports.signup_post = async (req, res) => {
    const { email, password } = req.body;
@@ -44,13 +44,13 @@ module.exports.signup_post = async (req, res) => {
       res.cookie('jwt', token, { httpOnly:true, maxAge: maxAge * 1000})
       res.status(200).json({ user: user._id })
    } catch (error) {
-      const errors = handleErrors(error)
-      res.status(400).json(errors)
-   }
-}
+      const errors = handleErrors(error);
+      res.status(400).json({ errors: errors });
+   };
+};
 
 module.exports.login_post = async (req, res) => {
    const { email, password } = req.body;
-   console.log(email, password)
-   res.send('user login')
-}
+   console.log(email, password);
+   res.send('user login');
+};
