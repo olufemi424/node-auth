b329d90db9d15a16aa43a5c6eaa618c2613e566d
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-08-26 22:33:17 -0500

    protect routes from unautho users

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const authRoutes = require('./routes/authRoute')
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoute');
+const { requireAuth } = require('./middleware/authMiddleware');
+
 
 dotenv.config({ path: './config/config.env' });
 
@@ -28,25 +30,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-// // cookies 
-// app.get('/set-cookies', (req, res) => {
-//   // res.setHeader('Set-Cookie', 'newUser=true')
-//   res.cookie('newUser', false)
-//   res.cookie('isEmployee', true, { 
-//     maxAge: 1000 * 60 * 60 * 24, 
-//     httpOnly: true,
-//     // secure: process.env.NODE_ENV !== 'production' // only over https in production
-//   })
-//   res.send('you got the cookies')
-
-// })
-
-// app.get('/read-cookies', (req, res) => {
-//   const cookies = req.cookies;
-
-//   console.log(cookies)
-//   res.json(cookies)
-// })
